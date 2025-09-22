4df1b74f5a3e5a64ff1c61ac99f804493ba3dc44
JoaoZanelato <jgbzanelato@gmail.com>
2025-09-22 17:04:54 -0300

    17:04

---
+++ dio-clone/src/components/Input.jsx
@@ -0,0 +1,11 @@
+import  {InputContainer, InputText, IconContainer} from './styles.js'
+
+const Input = ({leftIcon, name, ...rest}) => {
+  return <>
+  <InputContainer>
+    <InputText/>
+  </InputContainer>
+  </>;
+};
+
+export { Input };
