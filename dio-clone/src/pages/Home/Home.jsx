f5b015b1e5c5e7a5a4a3de2543c903f3c258e6b2
JoaoZanelato <jgbzanelato@gmail.com>
2025-09-22 16:11:49 -0300

    teste

--- dio-clone/src/pages/Home/Home.jsx
+++ dio-clone/src/pages/Home/Home.jsx
@@ -1,11 +1,33 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { Button } from "../../components/Button/Button";
 import { Header } from "../../components/Header/Header";
+import bannerImage from "../../assets/banner.png";
+import { Container, TextContent, Title, TitleHighlight } from "./styles.js";
 const Home = () => {
-  return(
+  return (
     <>
-    <Header/>
+      <Header />
+      <Container>
+        <div>
+          <Title>
+            <TitleHighlight>
+              Implemente
+              <br />
+            </TitleHighlight>
+            o seu futuro global agora!
+          </Title>
+          <TitleContent>
+            Domine as tecnologias utilizadas pelas empresas inovadoras do mundo
+            e encare seu novo desafio profisisonal, evoluind em comunidade com
+            os melhores experts.
+          </TitleContent>
+          <Button title="Começar agora" variant="secondary" onClick={() => null}/>
+        </div>
+        <div>
+          <img src={bannerImage} alt="banner" />
+        </div>
+      </Container>
     </>
-  )
-}
+  );
+};
 export default Home;
